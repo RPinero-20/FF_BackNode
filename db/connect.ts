043528b812888ea2ad8c1b89d4d82b8171e23a94
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2023-12-29 10:25:24 -0400

    Update connect.ts

--- db/connect.ts
+++ db/connect.ts
@@ -1,10 +1,16 @@
 import { Sequelize } from 'sequelize-typescript';
 
+// const db = new Sequelize('sql5673434', 'sql5673434', 'LpLS6lrBMj', {
+//     host: 'sql5.freesqldatabase.com',
+//     dialect: 'mysql',
+//     logging: false,
+// });
+
 
 const db = new Sequelize('f_ferlestore', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
-    //logging:false,
+    logging: false,
 });
 
 export async function conectToDB() {
