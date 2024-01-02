6765815d58ff0d92a68a79e3ab389a7bb071b612
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2024-01-02 16:12:39 -0400

    Update categories.ts

--- routes/categories.ts
+++ routes/categories.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { getCategories } from "../controller/categories";
+import { conectToDB } from "../db/connect";
 
 const categoriesRouter = Router();
 
 categoriesRouter.get('/', getCategories );
+
+categoriesRouter.use((_req, _res, next) => {
+    conectToDB();
+    next();
+});
 
 
 export default categoriesRouter;
